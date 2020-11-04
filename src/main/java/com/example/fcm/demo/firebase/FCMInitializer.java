996f362207b6df155554f5aebd63f89d12e2da0d
Seunghyeon-Kim <ssseungzz7@gmail.com>
2020-11-04 21:48:55 +0900

    Initial commit

---
+++ src/main/java/com/example/fcm/demo/firebase/FCMInitializer.java
@@ -0,0 +1,34 @@
+package com.example.fcm.demo.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Service
+public class FCMInitializer {
+
+    private static final String FIREBASE_CONFIG_PATH = "example-fcm-2bfdd-firebase-adminsdk-736on-654e307690";
+    Logger logger = (Logger) LoggerFactory.getLogger(FCMInitializer.class);
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream()))
+                    .build();
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}

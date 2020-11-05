48afb1e23a48c9bd8422b47605e50ba3fb51010b
Seunghyeon-Kim <ssseungzz7@gmail.com>
2020-11-05 16:05:53 +0900

    sendSampleNotification 메서드 추가

--- src/main/java/com/example/fcm/demo/controller/PushNotificationController.java
+++ src/main/java/com/example/fcm/demo/controller/PushNotificationController.java
@@ -1,8 +1,29 @@
 package com.example.fcm.demo.controller;
 
+import com.example.fcm.demo.model.PushNotificationResponse;
+import com.example.fcm.demo.service.PushNotificationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PushNotificationController {
+
+    private PushNotificationService pushNotificationService;
+
+    public PushNotificationController(PushNotificationService pushNotificationService) {
+        this.pushNotificationService = pushNotificationService;
+    }
+
+    @GetMapping("/notification")
+    public ResponseEntity sendSampleNotification() {
+        pushNotificationService.sendSamplePushNotification();
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(),
+                "Notification has been sent"), HttpStatus.OK);
+    }
+
+
 
 }
